455ab6e89f5aaa1b4f7f602ea07a25334356fa85
AWChristoph <caw@inismo.com>
2013-11-02 23:05:01 -0700

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nesthack::Application.config.secret_key_base = '5cd6dca84fa5c99931878dad4dacc4b5be12ce98baeb65152daf2acca735cad1aea0062795fcd7959167cdc8de35ba7e05966f5e25fa06299d9305a5510fa380'
